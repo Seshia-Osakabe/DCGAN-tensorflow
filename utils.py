e9bea92bdf0f68c7efdb8d7427f96eca2837ed40
Seshia-Osakabe <seshia.ofs@gmail.com>
2019-08-30 16:45:06 +0900

    sjdfajs

--- utils.py
+++ utils.py
@@ -90,8 +90,7 @@ def center_crop(x, crop_h, crop_w,
   h, w = x.shape[:2]
   j = int(round((h - crop_h)/2.))
   i = int(round((w - crop_w)/2.))
-  return scipy.misc.imresize(
-      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+  return numpy.array(Image.fromarray(x[j:j+crop_h, i:i+crop_w]).resize((resize_h, resize_w)))
 
 def transform(image, input_height, input_width, 
               resize_height=64, resize_width=64, crop=True):
